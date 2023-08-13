ab7fae93baed76d3814ecb19940bf2e5430eea7c
JeffersonYago <jefferson.almeida3@fatec.sp.gov.br>
2023-08-13 14:43:58 -0300

    Update SomaController.java

--- src/controller/SomaController.java
+++ src/controller/SomaController.java
@@ -5,8 +5,10 @@ public class SomaController {
 	}
 	public int soma(int n){
 	    if(n == 0)
+// quando n for igual a 0, a função deixará de ser chamada para ser realizada a soma dos números anteriores
 	        return 0;
 	    else
 	        return n + soma(n - 1);
+// toda a vez que a função é chamada, um número é adicionado á pilha para ser somado ao anterior
 	}
 }
